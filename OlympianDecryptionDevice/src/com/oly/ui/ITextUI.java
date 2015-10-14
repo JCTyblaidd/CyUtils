bc9e40f9f62caddfc2561c7989675533d456b737
JCTyblaidd <james@tyblaidd.co.uk>
2015-10-14 19:58:07 +0100

    PLEASE DON'T BREAK THIS TIME
    :warning:

---
+++ OlympianDecryptionDevice/src/com/oly/ui/ITextUI.java
@@ -0,0 +1,11 @@
+package com.oly.ui;
+
+public interface ITextUI extends IUI{
+
+	public void write(String str);
+	public void writeLine(String str);
+	public String getInput(); //RETURN NULL IF NOTHING
+	
+	
+	
+}
