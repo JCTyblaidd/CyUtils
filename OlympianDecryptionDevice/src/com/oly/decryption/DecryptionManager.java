6e0c377592f6f5abe99b6a8709c4e6f44d3d71ce
JCTyblaidd <james@tyblaidd.co.uk>
2015-10-14 18:14:41 +0100

    More Spin off wrapping
    :+1: still hardly anything to look at

---
+++ OlympianDecryptionDevice/src/com/oly/decryption/DecryptionManager.java
@@ -0,0 +1,24 @@
+package com.oly.decryption;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DecryptionManager {
+	
+	
+	public String target;
+	public List<IDecryptionOption> decrypters = new ArrayList<IDecryptionOption>();
+	
+	public DecryptionManager(String str) {
+		target = str;
+	}
+	
+	public void Analyse_And_Decrypt() {
+		
+	}
+	
+	
+	
+	
+	
+}
