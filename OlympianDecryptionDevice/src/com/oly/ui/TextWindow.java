70f517884cfa840d21b1cc4b590e40dac685d5c3
JCTyblaidd <james@tyblaidd.co.uk>
2015-10-14 21:26:47 +0100

    Added Text input/output color coordination
    yay!

--- OlympianDecryptionDevice/src/com/oly/ui/TextWindow.java
+++ OlympianDecryptionDevice/src/com/oly/ui/TextWindow.java
@@ -1,25 +1,26 @@
 package com.oly.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 public class TextWindow implements ITextUI{
 
 	public JFrame frame;
-	public JTextArea  output;
-	//public JFrame input_frame;
-	//public JPanel input_frame;
+	public JTextPane  output;
 	public JTextField input;
 	public JButton exec;
 	public JScrollPane scroll;
@@ -29,50 +30,52 @@ public class TextWindow implements ITextUI{
 	
 	public TextWindow(String name) {
 		frame = new JFrame(name);
-		output = new JTextArea(1,15);
-		//input_frame = new JPanel();
+		output = new JTextPane();
 		input = new JTextField();
 		exec = new JButton("Submit");
 		scroll = new JScrollPane (output, 
 				   JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		//
 		size(frame,680,430);
-		//size(output,680,350);
-		//size(input_frame,680,80);
-		//size(input,680,70);
-		//size(exec,60,60);
-		//size(scroll,30,350);]
 		scroll.setBounds(0, 0, 675, 365);
-		//input_frame.setBounds(0,350,680,80);
 		input.setBounds(5,370,570,25);
 		exec.setBounds(580,370,80,25);
 		//
-		//
-		//scroll.add(output);
 		frame.getContentPane().add(input,BorderLayout.WEST);
 		frame.getContentPane().add(exec,BorderLayout.EAST);
 		//
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().add(scroll,BorderLayout.NORTH);
-		//frame.getContentPane().add(input_frame,BorderLayout.SOUTH);
 		//
 		exec.addActionListener(new TextWindowHandler(this,input));
 		//
 		frame.setVisible(true);
-		//input_frame.setVisible(true);
+		output.setEditable(false);
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setMaximumSize(new Dimension(680,430));
 		frame.setMinimumSize(new Dimension(680,430));
 		frame.setResizable(false);
 		//
+		
+		output.setBackground(Color.WHITE);
+		
 	}
 	
 	private void size(Component comp,int h, int w) {
 		comp.setMaximumSize(new Dimension(h,w));
 		comp.setMinimumSize(new Dimension(h,w));
 		comp.setPreferredSize(new Dimension(h,w));
-		//comp.setBounds(r);
+	}
+	
+	private void writeWithColor(String str, Color c) {
+		StyledDocument doc = output.getStyledDocument();
+		Style style = output.addStyle("I'm a Style", null);
+        StyleConstants.setForeground(style, c);	
+        try { doc.insertString(doc.getLength(), str,style); }
+        catch (BadLocationException e){
+        	//LOLOLOLOLOL
+        }
 	}
 	
 	
@@ -85,7 +88,8 @@ public class TextWindow implements ITextUI{
 
 	@Override
 	public void write(String str) {
-		output.setText(output.getText() + str);
+		//output.setText(output.getText() + str);
+		writeWithColor(str,Color.BLACK);
 	}
 
 	@Override
@@ -117,9 +121,11 @@ public class TextWindow implements ITextUI{
 			String temp = input_box.getText();
 			input_box.setText("");
 			link_window.out_temp = temp;
-			link_window.writeLine(":USER: " + temp);
+			//link_window.writeLine("         " + temp);
+			writeWithColor("        "+temp+"\n",Color.RED);
 		}
 		
 	}
+	
 
 }
